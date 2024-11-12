fixed wrong order in numerical computation of an velocity

lrhcontrolenvs/envs/isaac_env.py
@@ -818,8 +818,9 @@ class IsaacSimEnv(LRhcEnvBase):
                 # differentiate numerically
                 self._root_v[robot_name][env_indxs, :] = (self._root_p[robot_name][env_indxs, :] - \
                                                 self._root_p_prev[robot_name][env_indxs, :]) / dt 
-                self._root_omega[robot_name][env_indxs, :] = quat_to_omega(self._root_q[robot_name][env_indxs, :], 
+                self._root_omega[robot_name][env_indxs, :] = quat_to_omega( 
                                                             self._root_q_prev[robot_name][env_indxs, :], 
+                                                            self._root_q[robot_name][env_indxs, :],
                                                             dt)
 
                 self._root_a[robot_name][env_indxs, :] = (self._root_v[robot_name][env_indxs, :] - \
@@ -859,9 +860,9 @@ class IsaacSimEnv(LRhcEnvBase):
                 # differentiate numerically
                 self._root_v[robot_name][:, :] = (self._root_p[robot_name][:, :] - \
                                                 self._root_p_prev[robot_name][:, :]) / dt 
-                self._root_omega[robot_name][:, :] = quat_to_omega(self._root_q[robot_name][:, :], 
-                                                            self._root_q_prev[robot_name][:, :], 
-                                                            dt)
+                self._root_omega[robot_name][:, :] = quat_to_omega(self._root_q_prev[robot_name][:, :], 
+                                                        self._root_q[robot_name][:, :], 
+                                                        dt)
 
                 self._root_a[robot_name][:, :] = (self._root_v[robot_name][:, :] - \
                                                 self._root_v_prev[robot_name][:, :]) / dt 
@@ -1089,7 +1090,6 @@ class IsaacSimEnv(LRhcEnvBase):
                                             clone = True) # root lin. velocityù
             self._root_v_base_loc[robot_name] = torch.full_like(self._root_v[robot_name], fill_value=0.0)
             self._root_v_prev[robot_name] = torch.full_like(self._root_v[robot_name], fill_value=0.0)
-
             self._root_v_default[robot_name] = torch.full_like(self._root_v[robot_name], fill_value=0.0)
             # root omega (measured, default)
             self._root_omega[robot_name] = self._robots_art_views[robot_name].get_angular_velocities(